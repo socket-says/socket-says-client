eab6823b95d4d723ae06673bf1d03ec9b66e1b8a
Brady Davenport <brady.davenport@pm.me>
2022-07-13 21:39:33 -0600

    refactoring

---
+++ __tests__/handleSequencePublish.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const handleSequencePublish = require('../../src/clients/player1/handleSequencePublish');
+
+describe('Tests for sequence publishing on player screen', () => {
+  test.skip('Server publishing initial sequence, displays that sequence on player screen', () => {
+    // Server publishing initial sequence
+
+    // displays that sequence on player screen
+  })
+  test.skip('Server publishing a non-initial sequence (2nd/3rd/4th), displays that sequence on player screen', () => {
+    // Server publishing a non-initial sequence (2nd/3rd/4th)
+
+    // displays that sequence on player screen
+  })
+});
---
+++ __tests__/handleWinLose.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const handleWinLose = require('../../src/clients/player1/handleWinLose');
+
+describe('Tests for client finishing a game', () => {
+  test('After client receives win/lose message, main-page prompt is displayed on client screen', () => {
+    // After client receives win/lose message
+
+    // expect(console.log).toEqual(''); // main-page prompt is displayed on client screen
+  })
+})
